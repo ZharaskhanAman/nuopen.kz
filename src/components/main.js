ceafa532ba3fc33c6e3b53777b6b5ba2632b2808
Zharaskhan <55748394+ZharaskhanAman@users.noreply.github.com>
2022-05-15 05:43:56 +0100

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -18,7 +18,7 @@ const Main = ({ formLink }) => {
         <Description>Join us on May 15, 2022 ONSITE to compete and have fun</Description>
       </div>
       <Button target="_blank" href="noopener noreferrer" href={formLink}>
-        Link to registration
+        Link to contest
       </Button>
     </Body>
   );
